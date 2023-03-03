6f822f35ce20f3e57324df545cdb5f1318ff673f
Mohammad Radwan <Gh93st@live.com>
2023-03-03 11:31:32 +0200

    created endpoint

--- Main.py
+++ Main.py
@@ -1,17 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 import pandas
 
 app = Flask(__name__)
 
 @app.route('/')
 def Index():
-    return "Irregualr Verbs API By Mr. mohammed Radwan"
+    return 'Irregualr Verbs API By Mr. mohammed Radwan'
 
-# df = pandas.read_csv("db.csv")
-# result = df.loc[df['Infinitive'] == 'be']
-# print(result.to_json())
+@app.get('/GetP')
+def GetParticiples():
+    srch = request.args.get('srch')
+    df = pandas.read_csv("db.csv")
+    result = df.loc[df['Infinitive'] == srch]
+    return result.to_json(orient="records")
 
-if (__name__ == '__name__'):
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+
+
+
 
 
